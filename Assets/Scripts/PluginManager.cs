5f37576fda5b36ca87267d08d3ce2efb7fb52d60
Thaidan Goguen-Bogdanis <thaidan.goguenbogdanis@ontariotechu.ne>
2020-10-08 17:34:06 -0400

    initial commit

---
+++ Assets/Scripts/PluginManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class PluginManager : MonoBehaviour
+{
+    const string DLL_NAME = "GameEnginesDLL";
+
+    //Methods
+    [DllImport(DLL_NAME)]
+    private static extern void ResetLogger();
+
+    //Setters
+    [DllImport(DLL_NAME)]
+    private static extern void SaveCheckpointTime(float RTBC);
+
+    //Getters
+    [DllImport(DLL_NAME)]
+    private static extern float GetTotalTime();
+
+    [DllImport(DLL_NAME)]
+    private static extern float GetCheckpointTime(int index);
+
+    [DllImport(DLL_NAME)]
+    private static extern int GetNumCheckpoints();
+
+    float lastTime = 0.0f;
+
+    public void SaveTimeTest(float checkpointTime)
+    {
+        SaveCheckpointTime(checkpointTime);
+    }
+
+    public float LoadTimeTest(int index)
+    {
+        if (index >= GetNumCheckpoints())
+        {
+            return -1.0f;
+        }
+
+        else
+        {
+            return GetCheckpointTime(index);
+        }
+    }
+
+    public float LoadTotalTimeTest()
+    {
+        return GetTotalTime();
+    }
+
+    public void ResetLoggerTest()
+    {
+        ResetLogger();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float currentTime = Time.time;
+            float checkpointTime = currentTime - lastTime;
+            lastTime = currentTime;
+
+            SaveTimeTest(checkpointTime);
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+            {
+                Debug.Log(LoadTimeTest(i));
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log(LoadTotalTimeTest());
+        }
+    }
+
+    private void OnDestroy()
+    {
+        ResetLoggerTest();
+    }
+}
