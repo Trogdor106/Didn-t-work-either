621d654c6cf224807301beb431168106537142c8
Thaidan Goguen-Bogdanis <thaidan.goguenbogdanis@ontariotechu.ne>
2020-10-09 15:37:45 -0400

    Moving Platform
    The platform at the end now moves back and forth between the maze and the end. The player is also placed next to it in this version, to see it happen.

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    float speed = 1.5f;
+    float totalTime = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, speed * Time.deltaTime); //Move object 1.5 units per second
+        totalTime += Time.deltaTime;
+
+        if (totalTime >= 5.0f)
+        {
+            speed *= -1.0f;
+            totalTime = 0.0f;
+        }
+    }
+}
